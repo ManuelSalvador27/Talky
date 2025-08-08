5ab7bba334c70b1adbe7c6a92a26a30a38cd3310
ManuelSalvador27 <manuelsalvador2799@gmail.com>
2025-08-08 13:51:17 -0600

    Add fallback avatar when contact image is not found

--- talky_v2/src/pages/chat/contacts.vue
+++ talky_v2/src/pages/chat/contacts.vue
@@ -1,11 +1,20 @@
 <template>
-<f7-page name="about">
+<f7-page name="Contacts">
     <f7-navbar title="Contacts" back-link="Back"></f7-navbar>
     <f7-list media-list>
-        <f7-list-item swipeout v-for="(contact, index) in contacts" :key="index" :title="contact.name">
-            <img class="small-avatar" slot="media" :src="contact.photo_url" />
+        <f7-list-item 
+            swipeout v-for="(contact, index) in contacts" 
+            :key="index" 
+            :title="contact.name"
+        >
+            <img
+                class="small-avatar" 
+                slot="media" 
+                :src="contact.photo_url" 
+                @error="setDefaultImage"
+            />
             <f7-swipeout-actions right>
-                <f7-swipeout-button color="green" @click="addFrd(contact)">Add</f7-swipeout-button>
+                <f7-swipeout-button color="green" @click="addFriend(contact)">Add</f7-swipeout-button>
             </f7-swipeout-actions>
         </f7-list-item>
 
@@ -15,6 +24,8 @@
 
 <script>
 import firebase from 'firebase'
+import defaultAvatar from '@/assets/defaultAvatar.png'
+
 export default {
     computed: {
         contacts() {
@@ -22,16 +33,22 @@ export default {
         }
     },
     methods: {
-        addFrd(frd) {
+        imgSrc(url) {
+            return url || defaultAvatar
+        },
+        setDefaultImage(e) {
+            e.target.src = defaultAvatar
+        },
+        addFriend(frd) {
             var request = {}
+            const self = this
             request.sender = firebase.auth().currentUser.uid;
             request.recipient = frd.uid
-            console.log('request',request)
+            console.log('request', request)
+            self.$store.commit('setAlertMessage', 'Friend request sent!')
             this.$store.dispatch('sendRequest', request)
-
         },
     },
-
     created() {
         this.$store.dispatch('getAllUsers')
     }
